2b7ab52a3db007738606eb4d267b9c83fea0198e
Manisha Kumari <krimanisha666@gmail.com>
2020-10-15 14:34:01 +0530

    Add addition and substraction file

---
+++ addition/manisha-kumari.py
@@ -0,0 +1,4 @@
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+add = int(x) + int(y)
+print("Addition: ", add)
---
+++ subtraction/manisha-kumari.py
@@ -0,0 +1,4 @@
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+sub = int(x) - int(y)
+print("Substraction: ", sub)
